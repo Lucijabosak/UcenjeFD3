2bf5a561b77742bfb85015aec622b4ff8b38f8b2
Lucijabosak <lucijabosak1@gmail.com>
2026-07-13 18:56:19 +0200

    JS

---
+++ 03_JS/vjezba01/skripta.js
@@ -0,0 +1,14 @@
+//alert('Hello world 1')
+
+
+document.getElementById('ispis').innerText = 'Hello world 6'
+document.getElementById('ispis').style.color = 'red'
+
+document.getElementById('ge').innerHTML =
+    '<a href="#">Ovo je poveznica</a>'
+
+console.log(`
+    Ovaj tekst mogu pisati kroz više redova
+    ..kasnije druge pogodnosti
+    `)
+
